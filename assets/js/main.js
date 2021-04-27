ab228516ebb3f90d126f97093160d651375780b9
bllllinel01 <binelimanga@gmail.com>
2021-04-28 00:06:19 +0100

    added responsive header

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,26 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+
+/* JS code to handle responsive header */
+function myFunction() {
+  let menus = document.getElementById("menus");
+  let menus_items = document.getElementsByClassName("menus__item");
+
+  if (menus.className == "menus") {
+    menus.className += " responsive";
+
+    for (const menu of menus_items) {
+      menu.className += " visible";
+    }
+  } 
+  else {
+    menus.className = "menus";
+
+    for (const menu of menus_items) {
+      menu.className = "menus__item";
+    }
+  }
+}
